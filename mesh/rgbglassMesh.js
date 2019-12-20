589d85a8dd084d33d0d148d6290276cdb21eb60f
caseyreed96 <59067227+caseyreed96@users.noreply.github.com>
2019-12-20 00:25:54 -0800

    i wish for freedom

--- mesh/rgbglassMesh.js
+++
@@ -1,8 +0,0 @@
-class RGBGlassMesh extends Mesh
-{
-	constructor(gl, program, positionArray, indexArray, normalArray, imageIDs, reflectionIntensity, refractionIntensity, refractiveIndex, diffuse, specular, ambient, shininess, position=new Vector(), rotation=new Quaternion(), scale=new Vector(1,1,1))
-	{
-		var material = new RGBGlassMaterial(gl, program, imageIDs, reflectionIntensity, refractionIntensity, refractiveIndex, diffuse, specular, ambient, shininess);
-		super(gl, program, positionArray, indexArray, normalArray, material, position, rotation, scale);
-	}
-}
--- threeparticles.js
+++ threeparticles.js
@@ -2,112 +2,124 @@ var camera;
 var scene;
 var renderer;
 var volcano;
-
 var clock;
 var deltaTime;
-
 var particleSystem;
+var theta=0;
+var radius=100;
 
 init();
 animate();
 
 function init(){
-
-	clock=new THREE.Clock(true);
-	
-    scene=new THREE.Scene();
-    camera=new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,1,1000);
-    camera.position.z=500;
-    
-    var light=new THREE.DirectionalLight(0xffffff);
-    light.position.set(1,-1,1).normalize();
-    //scene.add(light);
-     
-    particleSystem=createParticleSystem();
-    scene.add(particleSystem);
-    
-    renderer=new THREE.WebGLRenderer();
+	renderer=new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
+	window.addEventListener('resize',onWindowResize,false);
+    scene=new THREE.Scene();
 
-    window.addEventListener('resize',onWindowResize,false);
+    camera=new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,1,1000);
+
+    clock=new THREE.Clock(true);
+
+    particleSystem=createParticleSystem();
+    scene.add(particleSystem);
 
     initMesh();
+    initSkybox();
     render();
+    
 }
 
-function initMesh(){
+function initMesh()
+{
+	var volMaterial=new THREE.MeshBasicMaterial();
+	var volTexture;
 	var loader=new THREE.JSONLoader();
-	var volTexture=new THREE.TextureLoader().load('/textures/volctexture.png');
-	var volMaterial=new THREE.MeshBasicMaterial({map:volTexture});
+	var texLoader=new THREE.TextureLoader();
 	loader.load('/models/volc.json',function(geometry) {
-		volcano=new THREE.Mesh(geometry);
+		volcano=new THREE.Mesh(geometry,volMaterial);
 		volcano.scale.x=volcano.scale.y=volcano.scale.z=10;
 		volcano.position.set(0,-150,0);
 		scene.add(volcano);
 	});
+	texLoader.load('/textures/volctexture.png',function(texture)
+	{
+		volMaterial.map=texture;
+		volMaterial.needsUpdate=true;
+	});
 }
 
-function createParticleSystem() {
-	
-	// The number of particles in a particle system is not easily changed.
-    var particleCount = 2000;
-    
-    // Particles are just individual vertices in a geometry
-    // Create the geometry that will hold all of the vertices
-    var particles = new THREE.Geometry();
+function initSkybox()
+{
+	scene.background=new THREE.Color(0xf0f0f0); //why no work
+}
 
-	// Create the vertices and add them to the particles geometry
-	for (var p = 0; p < particleCount; p++) {
-	
-		// This will create all the vertices in a range of -200 to 200 in all directions
-		var x = Math.random() * 20-10;
-		var y = Math.random() * 300-50;
-		var z = Math.random() * 20-10;
-		      
-		// Create the vertex
-		var particle = new THREE.Vector3(x, y, z);
-		
-		// Add the vertex to the geometry
+function createParticleSystem()
+{
+    var particleCount = 2000;   
+    var particles = new THREE.Geometry();
+	for (var p = 0; p < particleCount; p++)
+	{
+		// This will create all the vertices in a specific range
+		var x=Math.random()*40-20;
+		var y=Math.random()*300-50;
+		var z=Math.random()*40-20;
+
+		var particle=new THREE.Vector3(x,y,z);
 		particles.vertices.push(particle);
 	}
 
-	// Create the material that will be used to render each vertex of the geometry
-	var particleMaterial = new THREE.PointsMaterial(
-			{color: 0xffffff, 
-			 size: 8,
-			 map: THREE.ImageUtils.loadTexture("textures/smoke.png"),
-			 blending: THREE.AdditiveBlending,
-			 transparent: true,
+	var particleMaterial=new THREE.PointsMaterial(
+			{color:0xffffff, 
+			 size:8,
+			 map:THREE.ImageUtils.loadTexture("textures/smoke.png"),
+			 blending:THREE.AdditiveBlending,
+			 transparent:true,
 			});
-	 
-	// Create the particle system
-	particleSystem = new THREE.Points(particles, particleMaterial);
 
+	particleSystem=new THREE.Points(particles,particleMaterial);
 	return particleSystem;	
 }
 
-function animate() {
+function animate()
+{
 	deltaTime = clock.getDelta();
     animateParticles();
     render();
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 }
 
-function animateParticles() {
+function animateParticles()
+{
 	var verts=particleSystem.geometry.vertices;
 	for(var i=0;i<verts.length;i++){
 		var vert=verts[i];
+		if(vert.x>20){
+			vert.x=Math.random()*40-20;
+		}
 		if(vert.y>150){
 			vert.y=Math.random()*150-50;
 		}
+		if(vert.z>20){
+			vert.z=Math.random()*40-20;
+		}
+		vert.x=vert.x+(10*deltaTime);
 		vert.y=vert.y+(10*deltaTime);
+		vert.z=vert.z+(10*deltaTime);
 	}
 	particleSystem.geometry.verticesNeedUpdate=true;
 	particleSystem.rotation.y-=.1*deltaTime;
 }
 
-function render(){
+function render()
+{
+	theta+=0.1;
+	camera.position.x=radius*Math.sin(THREE.Math.degToRad(theta));
+	camera.position.y=-50+radius*Math.sin(THREE.Math.degToRad(theta));
+	camera.position.z=100+radius*Math.cos(THREE.Math.degToRad(theta));
+	camera.lookAt(scene.position);
+
     renderer.render(scene,camera);
 }
 
